fee20fc8ada94d05a10767815294be9ae6d9637a
Manohin <111537419+Manohin@users.noreply.github.com>
2023-03-19 23:02:53 +0300

    Initial Commit

---
+++ JSONParsingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONParsingApp
+//
+//  Created by Alexey Manokhin on 19.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
