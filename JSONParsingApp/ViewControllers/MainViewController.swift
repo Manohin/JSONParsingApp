d3b94c0cb20a738dabaa04ab2db06be08d7641a3
Manohin <111537419+Manohin@users.noreply.github.com>
2023-03-20 03:49:55 +0300

    Done!

--- JSONParsingApp/ViewControllers/MainViewController.swift
+++ JSONParsingApp/ViewControllers/MainViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
     
     @IBOutlet var dogImageView: UIImageView!
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
